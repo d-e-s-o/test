8d65727b1513fcf72162a5924539e4fe82c79e12
Daniel Mueller <deso@posteo.net>
2021-04-12 21:31:06 -0700

    Merge me soon

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,8 @@
 // * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
 // *************************************************************************
 
+// foo bar
+
 #[cfg(test)]
 mod tests {
   #[test]
